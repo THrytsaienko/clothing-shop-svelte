a2745e19875a709bd160a6fcf4f047fab0f290a0
Tetiana <kortatiana89@gmail.com>
2021-10-13 00:53:14 +0300

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let name: string;
+	import Header from "./components/header/Header.svelte";
+</script>
+
+<main>
+	<Header />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 20px 60px;
+		max-width: 240px;
+		margin: 0 auto;
+		font-family: 'Open Sans Condensed', sans-serif;
+	}
+
+	@media screen and (max-width: 800px){
+		main {
+			padding: 10px;
+		}
+	}
+</style>
---
+++ src/components/header/Header.svelte
@@ -0,0 +1,11 @@
+<a href='/shop'>SHOP</a>
+<a href='/shop'>CONTACT</a>
+
+<style>
+    a {
+        padding: 10px 15px;
+        cursor: pointer;
+        text-decoration: none;
+        color: #000;
+    }
+</style>
